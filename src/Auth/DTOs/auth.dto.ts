5502451487e0b8cd710e0e57d4b57ae6dc903589
Abdelrahman <apoeljzzar@gmail.com>
2025-04-18 20:27:13 +0200

    auth dto

---
+++ src/Auth/DTOs/auth.dto.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { UserRoles } from "src/Common/Types/user.types";
+
+
+export class signUpDTO {
+    @IsString({ message: 'First name must be a string' })
+    @IsNotEmpty({ message: 'First name is required' })
+    @MinLength(3, { message: 'First name must be at least 3 characters long' })
+    firstName: string;
+
+    @IsString({ message: 'Last name must be a string' })
+    @IsNotEmpty({ message: 'Last name is required' })
+    @MinLength(3, { message: 'Last name must be at least 3 characters long' })
+    lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsString()
+    cPassword: string;
+
+    @IsEnum(UserRoles)
+    role: string;
+
+}
