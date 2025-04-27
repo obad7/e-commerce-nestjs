e3ff3df066d90238cd50eda4c01adc651a8622e0
Abdelrahman <apoeljzzar@gmail.com>
2025-04-27 19:04:42 +0300

    otp repo

--- src/DB/Repositories/otp.repo.ts
+++ src/DB/Repositories/otp.repo.ts
@@ -1,15 +1,32 @@
 import { Injectable } from "@nestjs/common";
 import { BaseRepo } from "./base.repo";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Model, Types } from "mongoose";
 import { Otp, OtpType } from "../Models/otp.model";
+import { OtpTypes } from "src/Common/Types/otp.types";
+import { Hash } from "src/Common/Security/hash.security";
 
+interface IcreateOtp {
+    userId: Types.ObjectId;
+    otp: string;
+    otpType: OtpTypes;
+    expireTime?: Date;
+}
 
 @Injectable()
 export class OtpRepo extends BaseRepo<OtpType> {
     constructor(
         @InjectModel(Otp.name) private readonly otpModel: Model<OtpType>,
-    ) { 
+    ) {
         super(otpModel);
+    }
+
+    async createOtp ( {userId, otp, otpType, expireTime}: IcreateOtp ) {
+        return await this.create({
+            userId,
+            otp: Hash(otp),
+            otpType,
+            expireTime: expireTime || new Date(Date.now() + 5 * 60 * 1000),
+        });
     }
 }
